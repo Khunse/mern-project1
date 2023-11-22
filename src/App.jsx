c743787a7accaf3316496816e6baead3a805f3c0
Khun Aung <yawyaw00898@gmail.com>
2023-11-22 16:57:37 +0630

    - add pages and routes ( home,login,signup,about )

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import SignUp from './pages/signup'
+import About from './pages/about'
+
 export default function App() {
   return (
-    <h1 className="text-green-700">
-      Hello world!dfdf
-    </h1>
+   <BrowserRouter>
+    <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/' element={<SignUp />}/>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
